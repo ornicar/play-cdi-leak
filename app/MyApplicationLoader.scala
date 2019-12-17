c77961156861b8a5cb5d6944666c8a3aae68cd6e
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-12-17 10:30:26 -0600

    minimal play compiletime DI project

---
+++ app/MyApplicationLoader.scala
@@ -0,0 +1,19 @@
+import play.api._
+import play.api.routing.Router
+
+class MyApplicationLoader extends ApplicationLoader {
+  def load(context: ApplicationLoader.Context): Application = {
+    new MyComponents(context).application
+  }
+}
+
+class MyComponents(context: ApplicationLoader.Context)
+    extends BuiltInComponentsFromContext(context)
+    with play.filters.HttpFiltersComponents {
+
+  lazy val homeController: _root_.controllers.HomeController = ???
+  // new _root_.controllers.HomeController(controllerComponents)
+
+  lazy val router: Router =
+    new _root_.router.Routes(httpErrorHandler, homeController)
+}
