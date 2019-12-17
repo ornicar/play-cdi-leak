0ad4f7d42ab795e899ee6b4fb5604a7336ebd06d
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-12-17 10:33:37 -0600

    comment bug

--- app/MyApplicationLoader.scala
+++ app/MyApplicationLoader.scala
@@ -11,8 +11,14 @@ class MyComponents(context: ApplicationLoader.Context)
     extends BuiltInComponentsFromContext(context)
     with play.filters.HttpFiltersComponents {
 
+  // this obviously fails at runtime, and then the MyComponents class
+  // remains forever loaded. There is no classloader GC between two `run`s.
+  // this eventually leads to OOM.
+  // It can be observed with:
+  // jmap -clstats $(jps | grep sbt-launch.jar | cut -f1 -d' ') | egrep MyComponents
+  // run the app, stop with enter, run again.
+  // Then jmap -clstats will show all classes loaded  twice.
   lazy val homeController: _root_.controllers.HomeController = ???
-  // new _root_.controllers.HomeController(controllerComponents)
 
   lazy val router: Router =
     new _root_.router.Routes(httpErrorHandler, homeController)
